92e67b917975a285f8a6bd4811806ac398030b68
Daquarius-E-Hicklebottom-the-13th <brannon.dempsey.984@sdjccs.net>
2025-03-14 09:50:22 -0700

    Added the suprise button

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
   // TODO #1: Confirm this ID matches the button in your HTML.
-  const magicButton = document.getElementById("button");
+  const magicButton = document.getElementById("magicButton");
 
   magicButton.addEventListener("click", function () {
     // TODO #2: Use any element selector you want here (e.g., .hero-section).
-    const heroSection = document.querySelector("");
+    const heroSection = document.querySelector(".hero-section");
 
     const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
     heroSection.style.backgroundColor = randomColor;
 
     // TODO #3: Customize the alert message text
     alert(
-      "Surprise! The hero section just changed color to " +
+      "Hicklebottom strikes again! I have changed the color to " +
       randomColor.toUpperCase()
     );
   });
